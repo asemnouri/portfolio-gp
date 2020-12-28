4d8e93047413ef0235fad66d9b59367f2c83d40a
asemnouri <asem_nori@hotmail.com>
2020-12-28 19:55:18 +0200

    try

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -52,13 +52,6 @@ export default function Profile() {
       getProfileData();
     }
   }, []);
-if (openSource.display && openSource.showGithubProfile === "true" && !(typeof prof === 'string' || prof instanceof String)){  
-    return (
-      <Suspense fallback={renderLoader()}>
-        <GithubProfileCard prof={prof} key={prof.id} /> 
-      </Suspense>
-       );
-  } else {
-    return <Contact />;
-  }
+
+    return( <Contact />);
 }
